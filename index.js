48547ede15caf3fd96c79a264bf4565af5226513
Manthan Joshi <37839298+manjosh9@users.noreply.github.com>
2020-02-24 18:14:40 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,30 @@
+var express = require('express');
+const path = require('path');
+var socket = require('socket.io');
+
+// App setup
+var app = express();
+var server = app.listen(4000, function(){
+    console.log('listening for requests on port 4000,');
+});
+
+// Static files
+app.use(express.static(path.join(__dirname,'public') ));
+
+// Socket setup & pass server
+var io = socket(server);
+io.on('connection', (socket) => {
+
+    console.log('made socket connection', socket.id);
+
+    // Handle chat event
+    socket.on('chat', function(data){
+        // console.log(data);
+        io.sockets.emit('chat', data);
+    });
+
+    socket.on('type', function(data) {
+      socket.broadcast.emit('type', data);
+    })
+
+});
